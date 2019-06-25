0918de1cf06d444f10c3ae5f8cc13669271e3bd6
huuuuusy <hushiyu1995@qq.com>
2019-06-25 15:19:27 +0800

    update190625

--- Project/Deep Learning/002-IMDB/01_Simple_DNN.py
+++ Project/Deep Learning/002-IMDB/01_Simple_DNN.py
@@ -27,11 +27,85 @@ print('the decoded first review: ', decoded_review)
 import numpy as np
 
 def vectorize_sequences(sequences, dimension = 10000):
-    results = np.zeros(len(sequences), dimension)
+    # 先构造一个全0矩阵
+    results = np.zeros((len(sequences), dimension))
+    # 然后将指定位置的元素置1
     for i, sequence in enumerate(sequences):
         results[i, sequence] = 1.
     return results
 
+# 将数据向量化
 x_train = vectorize_sequences(x_train)
 x_test = vectorize_sequences(x_test)
+print('train data shape: ', x_train.shape)
+print('test data shape: ', x_test.shape)
+print('the first vectorize review in train dataset: \n', x_train[0])
+print('the first vectorize review in test dataset: \n', x_test[0])
 
+# 将标签向量化
+y_train = np.asarray(y_train).astype('float32')
+y_test = np.asarray(y_test).astype('float32')
+
+# 构建神经网络
+from keras import models
+from keras import layers
+
+simple_dnn = models.Sequential()
+simple_dnn.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+simple_dnn.add(layers.Dense(16, activation='relu'))
+simple_dnn.add(layers.Dense(1, activation='sigmoid'))
+
+# 自定义损失函数和指标
+from keras import losses
+from keras import metrics
+from keras import optimizers
+
+simple_dnn.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy, metrics=[metrics.binary_accuracy])
+
+# 测试神经网络
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = simple_dnn.fit(partial_x_train,
+                        partial_y_train,
+                        epochs=4,
+                        batch_size=512,
+                        validation_data=(x_val, y_val))
+
+# 绘制图像
+# 训练损失和验证损失
+import matplotlib.pyplot as plt
+
+val_loss = history.history['val_loss']
+val_binary_accuracy = history.history['val_binary_accuracy']
+loss = history.history['loss']
+binary_accuracy = history.history['binary_accuracy']
+
+epochs = range(1, len(binary_accuracy)+1)
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# 训练准确率和验证准确率
+plt.clf()   # clear figure
+
+plt.plot(epochs, binary_accuracy, 'bo', label='Training acc')
+plt.plot(epochs, val_binary_accuracy, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# 测试集准确率
+results = simple_dnn.evaluate(x_test, y_test)
+print('evaluate test result:', results)
---
+++ Project/Deep Learning/003-路透社新闻分类/01_Simple_DNN.py
@@ -0,0 +1,109 @@
+""" 
+@Author: huuuuusy
+@GitHub: https://github.com/huuuuusy
+
+Reuters数据集是新闻的多分类文本数据集，有46个主题
+每个主题的数量不同，但是每个主题的训练集中至少有10个样本
+"""
+
+# 下载数据
+from keras.datasets import reuters
+# 取出现频率最高的10000词构成词典，将数据中的单词用索引值表示
+(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=10000)
+print(len(x_train))
+print(len(x_test))
+print('the first data:\n', x_train[0])
+
+# 将索引解析为单词
+word_index = reuters.get_word_index()
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+decoded_review = ' '.join([reverse_word_index.get(i-3, '?') for i in x_train[0]])
+print('the first decode data:\n', decoded_review)
+
+# 准备数据
+import numpy as np 
+
+# 对训练数据和测试数据进行one-hot编码
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorize_sequences(x_train)
+x_test = vectorize_sequences(x_test)
+
+# 对labels进行one-hot编码
+def to_one_hot(labels, dimension=46):
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels):
+        results[i, label] = 1. 
+    return results
+
+y_train = to_one_hot(y_train)
+y_test = to_one_hot(y_test)
+
+print('train data shape: ', x_train.shape)
+print('test data shape: ', x_test.shape)
+print('train label shape: ', y_train.shape)
+print('test label shape: ', y_test.shape)
+
+# 构建神经网络
+
+from keras import layers
+from keras import models
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+
+model.compile(optimizer='rmsprop', 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
+
+# 测试神经网络
+# 取1000个样本作为训练时的验证集，剩下的作为训练集
+x_val = x_train[:1000]
+x_train = x_train[1000:]
+y_val = y_train[:1000]
+y_train = y_train[1000:]
+
+history = model.fit(x_train, y_train,
+                    epochs=8,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+# 绘图 
+# 训练损失和验证损失
+import matplotlib.pyplot as plt 
+
+val_loss = history.history['val_loss']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+acc = history.history['acc']
+
+epochs = range(1, len(acc)+1)
+
+plt.plot(epochs, loss, 'bo',  label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation liss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# 训练准确率和测试准确率
+plt.clf()   # clear figure
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+# 测试集准确率
+results = model.evaluate(x_test, y_test)
+print('evaluate test result:', results)
