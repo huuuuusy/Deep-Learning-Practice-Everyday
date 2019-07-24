27d71b88cc18aff19dc1a77db6feda4240d27700
huuuuusy <hushiyu1995@qq.com>
2019-07-24 10:35:53 +0800

    190724

--- Data Science/001-Python内建数据结构/05_Set.py
+++ Data Science/001-Python内建数据结构/05_Set.py
@@ -26,11 +26,19 @@ print('Example 43:')
 print(a.intersection(b))
 print(a & b)
 
-
+# 集合元素不可变，如果想要包含列表型元素，需要转换为元组
 print('Example 44:')
+my_data = [1, 2, 3, 4]
+my_set = {tuple(my_data)}
+print(my_set)
 
+# 判断一个集合是否为另一个集合的子集或者超集
 print('Example 45:')
+a_set = {1, 2, 3, 4, 5}
+print({1, 2, 3}.issubset(a_set))
+print(a_set.issuperset({1, 2, 3}))
 
+# 判断集合是否相等
 print('Example 46:')
+print({1, 2, 3} == {3, 2, 1})
 
-print('Example 47:')
