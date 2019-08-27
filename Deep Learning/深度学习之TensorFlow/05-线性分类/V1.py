c6549acb91330e05d494c957fa6eab7a8a2f2ed5
huuuuusy <hushiyu1995@qq.com>
2019-08-27 17:52:03 +0800

    190827

---
+++ Deep Learning/深度学习之TensorFlow/05-线性分类/V1.py
@@ -0,0 +1,16 @@
+"""
+@Author: huuuuusy
+@GitHub: https://github.com/huuuuusy
+
+系统： Ubuntu 18.04
+IDE:  VS Code 1.37
+框架： TensorFlow-GPU == 1.13.1
+介绍： 线性单分类问题分析
+"""
+
+"""
+肿瘤类别判断问题
+    生成肿瘤数据，并判断新的数据是属于良性肿瘤还是恶性肿瘤
+    数据的特征包括病人的年龄和肿瘤大小
+    标签为良性肿瘤或者恶性肿瘤
+"""
